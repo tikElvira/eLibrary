943aad5c3ec7a8a2a5b564c426def58640a64e5f
tikElvira <tikelvira@gmail.com>
2023-06-09 16:19:07 +0200

    Initial commit

---
+++ admin/includes/menubar.php
@@ -0,0 +1,65 @@
+<aside class="main-sidebar">
+  <!-- sidebar: style can be found in sidebar.less -->
+  <section class="sidebar">
+    <!-- Sidebar user panel -->
+    <div class="user-panel">
+      <div class="pull-left image">
+        <img src="<?php echo (!empty($user['photo'])) ? '../images/'.$user['photo'] : '../images/profile.jpg'; ?>" class="img-circle" alt="User Image">
+      </div>
+      <div class="pull-left info">
+        <p><?php echo $user['firstname'].' '.$user['lastname']; ?></p>
+        <a><i class="fa fa-circle text-success"></i> Online</a>
+      </div>
+    </div>
+    <!-- sidebar menu: : style can be found in sidebar.less -->
+    <ul class="sidebar-menu" data-widget="tree">
+      <li class="header">REPORTS</li>
+      <li class=""><a href="home.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
+      <li class="header">MANAGE</li>
+      <li class="treeview">
+        <a href="#">
+          <i class="fa fa-refresh"></i>
+          <span>Librat Fizik</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="borrow.php"><i class="fa fa-circle-o"></i> Huazuar</a></li>
+          <li><a href="return.php"><i class="fa fa-circle-o"></i> Kthyer</a></li>
+        </ul>
+      </li>
+      <li class="treeview">
+        <a href="#">
+          <i class="fa fa-book"></i>
+          <span>Librat</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="book.php"><i class="fa fa-circle-o"></i> Lista e Librave</a></li>
+          <li><a href="category.php"><i class="fa fa-circle-o"></i> Kategoria</a></li>
+        </ul>
+      </li>
+      <li class="treeview">
+        <a href="#">
+          <i class="fa-solid fa-users"></i>
+          <span>Nxenesit</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="student.php"><i class="fa fa-circle-o"></i> Lista e Nxenesve</a></li>
+          <li><a href="course.php"><i class="fa fa-circle-o"></i> Course</a></li>
+        </ul>
+      </li>
+
+      
+      <li class=""><a href="matura.php"><i class="fa fa-graduation-cap"></i> <span>Matura Shteterore</span></a></li>
+    </ul>
+
+  </section>
+  <!-- /.sidebar -->
+</aside>
